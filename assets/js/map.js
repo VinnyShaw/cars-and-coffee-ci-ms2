6f453c00b5b54250097faa57c1e62dcc0bdc5451
Vinny Shaw <VinnyShaw@users.noreply.github.com>
2021-05-23 18:20:23 +0100

    Google map render script

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,32 +1,10 @@
+// Renders the map centered at Inverness, Scotland.
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
+    zoom: 8.5,
     center: {
       lat: 57.4679914,
       lng: -4.256877,
     },
-  });
-
-  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  var locations = [
-    //   Rollerbowl
-    { lat: 57.4508937, lng: -4.2249295 },
-    // The Dores Inn
-    { lat: 57.3819217, lng: -4.3353143 },
-    // Fort Augustus
-    { lat: 57.1443685, lng: -4.6928865 },
-  ];
-
-  var markers = locations.map(function (location, i) {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
-    });
-  });
-
-  var markerCluster = new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
