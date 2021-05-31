302ca2a01c2a3f542aa0bf60be97acc44576e507
Vinny Shaw <VinnyShaw@users.noreply.github.com>
2021-05-31 09:19:59 +0100

    nav anchor fix emailjs testing

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,13 +3,13 @@
 console.log("This means email.js is linked correctly");
 
 function sendMail(contactForm) {
-  emailjs
-    .send("gmail", "ms2", {
-      "name": contactForm.name.value,
-      "from_email": contactForm.emailaddress.value,
-      "subject": contactForm.subject.value,
-      "message": contactForm.message.value,
-    })
+  console.log("This means emailjs.send function called");
+  emailjs.send("gmail", "ms2", {
+    "name": contactForm.name.value,
+    "from_email": contactForm.emailaddress.value,
+    "subject": contactForm.subject.value,
+    "message": contactForm.message.value,
+  })
     .then(
       function (response) {
         console.log("SUCCESS", response);
@@ -22,6 +22,4 @@ function sendMail(contactForm) {
 }
 console.log("This is after the emailjs function");
 
-// todo: Go back thru emailjs vids and match my form to the one from and match emailjs template too and get it all working
-// todo: perhaps look at combining first and last name fields for ease?
-// todo: check the use of from in ci exampkle code for html and js
+// todo: Why does this work offline but not live???
