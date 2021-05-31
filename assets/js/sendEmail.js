cfaebf9f00aa53f9d8b8e8a8a8c5b1345a4ccdd7
Vinny Shaw <VinnyShaw@users.noreply.github.com>
2021-05-31 08:56:09 +0100

    testing work done to emailjs functionality

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,12 +1,14 @@
 // todo: index.html is linking to email.js correctly but no email received
+
 console.log("This means email.js is linked correctly");
+
 function sendMail(contactForm) {
   emailjs
     .send("gmail", "ms2", {
-      from_name: contactForm.name.value,
-      from_email: contactForm.emailaddress.value,
-      from_subject: contactForm.projectsummary.value,
-      from_message: contactForm.message.value,
+      "name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "subject": contactForm.subject.value,
+      "message": contactForm.message.value,
     })
     .then(
       function (response) {
@@ -20,5 +22,6 @@ function sendMail(contactForm) {
 }
 console.log("This is after the emailjs function");
 
-// todo: Go back thru emailjs vids and match my form to theit from and match emailjs template too and get it all working
+// todo: Go back thru emailjs vids and match my form to the one from and match emailjs template too and get it all working
 // todo: perhaps look at combining first and last name fields for ease?
+// todo: check the use of from in ci exampkle code for html and js
